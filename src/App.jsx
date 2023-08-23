b2b99fc5edf2a717a5891a40313a3d74112e473f
RydKrm <alriyadkarim@gmail.com>
2023-08-23 19:58:44 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2 className='text-center text-3xl text-green-300 '>EmpowerRise client site </h2>
+    </>
+  )
+}
+
+export default App
