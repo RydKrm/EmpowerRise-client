7d41a31a9b450c8633a93f5cd8cda926bd38dc70
Abu-Sahad <abusahad603@gmail.com>
2023-09-19 11:34:05 +0600

    update navbar

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -31,17 +31,6 @@ const NavBar = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
                         <li><Link to='addCategory'>Add Category</Link></li>
                         <li><Link to='Donation'> Donation</Link></li>
                         <li><Link to='Fund'> Fund</Link></li>
